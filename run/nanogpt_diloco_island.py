7431f8193e339c57bc70f1e4f0b4d7d02f9445cf
Matt Beton <matthew.beton@gmail.com>
2025-03-21 09:34:49 +0000

    Reverted back to diloco / diloco_island separation

--- run/nanogpt_diloco_island.py
+++ run/nanogpt_diloco_island.py
@@ -6,7 +6,7 @@ import numpy as np
 from DistributedSim.sim_builder import *
 from DistributedSim.sim_config import *
 from DistributedSim.gradient_strategy.gradient_strategy import *
-from DistributedSim.gradient_strategy.diloco_gradient import *
+from DistributedSim.gradient_strategy.diloco_island_gradient import *
 
 from DistributedSim.models.nanogpt import GPT, GPTConfig
 from DistributedSim.dataset.build_dataset import *
@@ -20,14 +20,18 @@ def main():
     parser.add_argument('--outer_lr', type=float, default=0.7)
     parser.add_argument("--nesterov", type=bool, default=True)
     parser.add_argument("--outer_momentum", type=float, default=0.9)
+    parser.add_argument("--island_size", type=int, default=None)
 
     args = parser.parse_args()
+
+    if args.island_size is None:
+        args.island_size = args.num_nodes
 
     gpt_config = gen_gpt_config(args)
 
     config = config_gen(args, gpt_config)
 
-    config.gradient_class = DiLoCoGradient
+    config.gradient_class = DiLoCoIslandGradient
     config.gradient_config.diloco_interval = args.diloco_interval
     config.gradient_config.outer_optimizer_cls = torch.optim.SGD
     config.gradient_config.outer_optimizer_kwargs = {
@@ -35,6 +39,7 @@ def main():
         'nesterov': args.nesterov,
         'momentum': args.outer_momentum,
     }
+    config.gradient_config.island_size = args.island_size
 
     simbuilder = LocalSimBuilder(config)
 
